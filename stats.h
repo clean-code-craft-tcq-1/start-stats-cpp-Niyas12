60936b1d12637723eac7084e6a6f123b692ef6a9
Niyas12 <77484230+Niyas12@users.noreply.github.com>
2021-01-28 08:09:30 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,5 +1,45 @@
 #include <vector>
 
-namespace Statistics {
-    //Stats ComputeStatistics(const std::vector<___>& );
-}
+typedef struct Stats
+{
+    float f32Average;
+    float f32Min;
+    float f32Max;
+};
+
+class IAlerter
+{
+
+public:
+    virtual void DoAlert() = 0;
+};
+
+class EmailAlert: public IAlerter
+{
+public:
+    bool emailSent = false;
+    virtual void DoAlert();
+};
+class LEDAlert : public IAlerter
+{
+public:
+    bool ledGlows = false;
+    virtual void DoAlert();
+};
+
+class StatsAlerter
+{
+private:
+    float m_maxThreshold;
+    std::vector<IAlerter*> m_alerters;
+public:
+    StatsAlerter(float maxThreshold, std::vector<IAlerter*> alerters);
+    void checkAndAlert(const std::vector<float>& data);
+};
+
+class Statistics
+{
+    static Stats m_sStat;
+public:
+    static Stats ComputeStatistics(const std::vector<float>& data);
+};
